e2a69ef435a7b916d5e002eac6f870320a194fce
avijeet108 <kumaravijeet108@gamil.com>
2021-10-09 00:39:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        primaryIconTheme: IconThemeData(color: Colors.black),
+        primaryTextTheme: TextTheme(
+            title: TextStyle(color: Colors.black, fontFamily: 'Aveny')),
+        textTheme: TextTheme(title: TextStyle(color: Colors.black)),
+      ),
+      home: Home(),
+    );
+  }
+}
